917abfa8d0281ed461ac29919073cad09ea9684f
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-02-23 21:15:29 +0530

    Using groq-sdk

--- invoking-llms/app.js
+++ invoking-llms/app.js
@@ -1,2 +1,26 @@
 // console.log("GenAI is fun!");
+import Groq from "groq-sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
+const GROQ_API_KEY = process.env.GROQ_API_KEY;
+
+const groq = new Groq({
+    apiKey: GROQ_API_KEY,
+});
+
+async function main() {
+    const chatCompletion = await groq.chat.completions
+        .create({
+            model: "llama-3.3-70b-versatile",
+            messages: [
+                {
+                    role: "user",
+                    content: "Explain the importance of fast language models"
+                }
+            ]
+        });
+    console.log(chatCompletion.choices[0].message.content);
+};
+
+main();

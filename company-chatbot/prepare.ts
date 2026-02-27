d266abf2fc6fe6dadcda724d9f4a92bc74f32317
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-02-27 19:34:08 +0530

    2. Chunk the documents

--- company-chatbot/prepare.ts
+++ company-chatbot/prepare.ts
@@ -1,8 +1,21 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-export async function indexTheDocument(filePath: string){
-    const loader = new PDFLoader(filePath);
+export async function indexTheDocument(filePath: string) {
+    const loader = new PDFLoader(filePath, { splitPages: false });
     const docs = await loader.load();
-    
-    console.log(docs);
+
+    // console.log(docs[0]?.pageContent);
+
+    if (!docs[0]?.pageContent) {
+        throw new Error("No content found in the document");
+    }
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,      // Max characters per chunk
+        chunkOverlap: 100,    // Overlap between adjacent chunks to maintain context
+    });
+
+    const chunks = await splitter.splitText(docs[0].pageContent);
+    console.log(chunks.length);
 }

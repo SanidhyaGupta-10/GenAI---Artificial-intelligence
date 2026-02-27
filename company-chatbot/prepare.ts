1574b16894b477b1e0d7f2ba935cd7edf106ac7d
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-02-27 20:20:15 +0530

    feat: Add initial document preparation and indexing using Langchain, OpenAI embeddings, and Pinecone.

--- company-chatbot/prepare.ts
+++ company-chatbot/prepare.ts
@@ -1,5 +1,21 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+const embeddings = new OpenAIEmbeddings({
+    model: "text-embedding-3-small",
+    // dimensions: 1536,
+});
+
+const pinecone = new PineconeClient({
+    apiKey: process.env.PINECONE_API_KEY!,
+});
+const pineconeStore = new PineconeStore(embeddings, {
+    pineconeIndex: pinecone.Index("company-chatbot"),
+});
+
 
 export async function indexTheDocument(filePath: string) {
     const loader = new PDFLoader(filePath, { splitPages: false });
